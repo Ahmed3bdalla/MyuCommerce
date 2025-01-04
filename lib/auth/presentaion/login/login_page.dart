e7f4c5cf783287287b26b5969551c18b0507cb98
Ahmed3bdalla <ss12449955@gmail.com>
2025-01-04 17:23:07 +0200

    any thing

--- lib/auth/presentaion/login/login_page.dart
+++ lib/auth/presentaion/login/login_page.dart
@@ -1,154 +1,84 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get/get.dart';
-import 'package:myucommerce/auth/presentaion/login/bloc/login_bloc.dart';
-import 'package:myucommerce/helper/constants.dart';
-import '../../../view/widgets/custom_textfield.dart';
+import 'package:myucommerce/auth/presentaion/signup/signup_page.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    final bloc = LoginBloc();
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      backgroundColor: Colors.white,
-      body: ListView(
-        children: [
-          SizedBox(
-            height: Get.height * 0.1,
-          ),
-          Image.asset(
-            kLogo,
-            height: Get.height * 0.2,
-            width: Get.width,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(
-            height: Get.height * 0.03,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Text(
-              'Login',
-              style: TextStyle(fontSize: 22),
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          const Padding(
-            padding: EdgeInsets.only(left: 20.0),
-            child: Text(
-              'Welcome Back',
-            ),
-          ),
-          SizedBox(
-            height: Get.height * 0.03,
-          ),
-          Column(
-            children: [
-              StreamBuilder<String>(
-                  stream: bloc.emailError,
-                  builder: (context, snapshot) {
-                    return MyTextField(
-                      errorText: snapshot.data,
-                      icon: const Icon(Icons.person),
-                      onChanged: (email) {
-                        bloc.add(UpdateEmail(email: email));
-                      },
-                      // validator: (value) {
-                      //   if (value == null || value.isEmpty) {
-                      //     return 'Please enter email';
-                      //   } else if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
-                      //     return "Enter a valid Email";
-                      //   }
-                      //   return null;
-                      // },
-                      hintText: 'Email',
-                    );
-                  }),
-              SizedBox(
-                height: Get.height * 0.02,
-              ),
-              StreamBuilder<String>(
-                  stream: bloc.passError,
-                  builder: (context, snapshot) {
-                    return MyTextField(
-                      errorText: snapshot.data,
-                      isPassword: true,
-                      onChanged: (pass) {
-                        bloc.add(UpdatePass(pass: pass));
-                      },
-                      // validator: (value) {
-                      //   if (value == null || value.isEmpty) {
-                      //     return 'Please enter Password';
-                      //   }
-                      //   return null;
-                      // },
-                      icon: const Icon(Icons.lock_open),
-                      // controller: bloc.passController,
-                      hintText: 'Password',
-                    );
-                  }),
-              SizedBox(
-                height: Get.height * 0.01,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      appBar: AppBar(title: const Text("Credit Card Comparison")),
+      body: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal, // Enables horizontal scrolling
+          child: ConstrainedBox(
+            constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical, // Enables vertical scrolling
+              child: Table(
+                defaultColumnWidth: const IntrinsicColumnWidth(),
+                border: TableBorder.all(color: Colors.purple),
                 children: [
-                  const Text(
-                    'If you have not an Account',
-                    textAlign: TextAlign.center,
-                  ),
-                  TextButton(
-                      onPressed: () {
-                        Get.offAllNamed('/register');
-                      },
-                      child: const Text(
-                        'Register',
-                        style: TextStyle(
-                            color: kPrimaryColor, fontWeight: FontWeight.bold),
-                      ))
+                  _buildRow('Attribute', 'Credit Card 1', 'Credit Card 2'),
+                  _buildRow('Annual Fee', '\$95', '\$0'),
+                  _buildRow('APR', '13.99%', '15.99%'),
+                  _buildRow('Cashback', '2%', '1.5%'),
+                  _buildRow('Sign-up Bonus', '\$300', '\$200'),
+                  _buildRow('Attribute', 'Credit Card 1', 'Credit Card 2'),
+                  _buildRow('Annual Fee', '\$95', '\$0'),
+                  _buildRow('APR', '13.99%', '15.99%'),
+                  _buildRow('Cashback', '2%', '1.5%'),
+                  _buildRow('Sign-up Bonus', '\$300', '\$200'),
+                  _buildRow('Attribute', 'Credit Card 1', 'Credit Card 2'),
+                  _buildRow('Annual Fee', '\$95', '\$0'),
+                  _buildRow('APR', '13.99%', '15.99%'),
+                  _buildRow('Cashback', '2%', '1.5%'),
+                  _buildRow('Sign-up Bonus', '\$300', '\$200'),
+                  _buildRow('Attribute', 'Credit Card 1', 'Credit Card 2'),
+                  _buildRow('Annual Fee', '\$95', '\$0'),
+                  _buildRow('APR', '13.99%', '15.99%'),
+                  _buildRow('Cashback', '2%', '1.5%'),
+                  _buildRow('Sign-up Bonus', '\$300', '\$200'),
+
+                  // Add more rows as needed
                 ],
               ),
-              SizedBox(
-                height: Get.height * 0.02,
-              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
-              /// Login Button
-              BlocBuilder<LoginBloc, LoginState>(
-                  bloc: bloc,
-                  builder: (context, state) {
-                    return SizedBox(
-                      width: double.infinity,
-                      height: 55,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor:
-                              WidgetStateProperty.all(kPrimaryColor),
-                          shape: WidgetStateProperty.all(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
-                            ),
-                          ),
-                        ),
-                        onPressed: () {
-                          bloc.add(Login());
-                        },
-                        child: state is LoginLoadingState
-                            ? const CircularProgressIndicator(
-                                color: Colors.white,
-                              )
-                            : const Text(
-                                "Login",
-                                style: TextStyle(color: Colors.white),
-                              ),
-                      ),
-                    );
-                  })
-            ],
-          ).paddingSymmetric(horizontal: 20),
+  TableRow _buildRow(String attribute, String card1Value, String card2Value, {bool isHeader = true}) {
+    const TextStyle headerStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white);
+    const Color headerBackgroundColor = Colors.purple; // Choose your header color
+    const Color defaultBackgroundColor = Colors.transparent;
+
+    return TableRow(
+      children: [
+        _buildCell(attribute, isHeader, headerStyle, headerBackgroundColor),
+        _buildCell(card1Value, false, headerStyle, defaultBackgroundColor),
+        _buildCell(card2Value, false, headerStyle, defaultBackgroundColor),
+      ],
+    );
+  }
+
+  Widget _buildCell(String text, bool isHeader, TextStyle headerStyle, Color headerBackgroundColor) {
+    return Container(
+      decoration: BoxDecoration(
+        border: const Border(
+            bottom: BorderSide(
+          color: Colors.white,
+          width: 1.0,
+        )),
+        color: isHeader ? Colors.purple : Colors.transparent,
+      ),
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          Text(text, style: isHeader ? headerStyle : null),
         ],
       ),
     );
