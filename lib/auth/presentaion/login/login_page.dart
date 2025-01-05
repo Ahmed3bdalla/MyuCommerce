7939778b1f79cb790269725e16957d74f4d7f7c8
Ahmed3bdalla <ss12449955@gmail.com>
2025-01-05 13:54:34 +0200

    add main.yml

--- lib/auth/presentaion/login/login_page.dart
+++ lib/auth/presentaion/login/login_page.dart
@@ -7,80 +7,17 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Credit Card Comparison")),
-      body: ClipRRect(
-        borderRadius: BorderRadius.circular(20),
-        child: SingleChildScrollView(
-          scrollDirection: Axis.horizontal, // Enables horizontal scrolling
-          child: ConstrainedBox(
-            constraints: BoxConstraints(minWidth: MediaQuery.of(context).size.width),
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical, // Enables vertical scrolling
-              child: Table(
-                defaultColumnWidth: const IntrinsicColumnWidth(),
-                border: TableBorder.all(color: Colors.purple),
-                children: [
-                  _buildRow('Attribute', 'Credit Card 1', 'Credit Card 2'),
-                  _buildRow('Annual Fee', '\$95', '\$0'),
-                  _buildRow('APR', '13.99%', '15.99%'),
-                  _buildRow('Cashback', '2%', '1.5%'),
-                  _buildRow('Sign-up Bonus', '\$300', '\$200'),
-                  _buildRow('Attribute', 'Credit Card 1', 'Credit Card 2'),
-                  _buildRow('Annual Fee', '\$95', '\$0'),
-                  _buildRow('APR', '13.99%', '15.99%'),
-                  _buildRow('Cashback', '2%', '1.5%'),
-                  _buildRow('Sign-up Bonus', '\$300', '\$200'),
-                  _buildRow('Attribute', 'Credit Card 1', 'Credit Card 2'),
-                  _buildRow('Annual Fee', '\$95', '\$0'),
-                  _buildRow('APR', '13.99%', '15.99%'),
-                  _buildRow('Cashback', '2%', '1.5%'),
-                  _buildRow('Sign-up Bonus', '\$300', '\$200'),
-                  _buildRow('Attribute', 'Credit Card 1', 'Credit Card 2'),
-                  _buildRow('Annual Fee', '\$95', '\$0'),
-                  _buildRow('APR', '13.99%', '15.99%'),
-                  _buildRow('Cashback', '2%', '1.5%'),
-                  _buildRow('Sign-up Bonus', '\$300', '\$200'),
-
-                  // Add more rows as needed
-                ],
-              ),
-            ),
-          ),
+        appBar: AppBar(
+          title: const Text('Login Page'),
         ),
-      ),
-    );
-  }
-
-  TableRow _buildRow(String attribute, String card1Value, String card2Value, {bool isHeader = true}) {
-    const TextStyle headerStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Colors.white);
-    const Color headerBackgroundColor = Colors.purple; // Choose your header color
-    const Color defaultBackgroundColor = Colors.transparent;
-
-    return TableRow(
-      children: [
-        _buildCell(attribute, isHeader, headerStyle, headerBackgroundColor),
-        _buildCell(card1Value, false, headerStyle, defaultBackgroundColor),
-        _buildCell(card2Value, false, headerStyle, defaultBackgroundColor),
-      ],
-    );
-  }
-
-  Widget _buildCell(String text, bool isHeader, TextStyle headerStyle, Color headerBackgroundColor) {
-    return Container(
-      decoration: BoxDecoration(
-        border: const Border(
-            bottom: BorderSide(
-          color: Colors.white,
-          width: 1.0,
-        )),
-        color: isHeader ? Colors.purple : Colors.transparent,
-      ),
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          Text(text, style: isHeader ? headerStyle : null),
-        ],
-      ),
-    );
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () => throw Exception(),
+              child: const Text("Throw Test Exception"),
+            ),
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:ui';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
@@ -20,6 +23,14 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  FlutterError.onError = (errorDetails) {
+    FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+  };
+  // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   await Hive.initFlutter();
   await Hive.openBox('order');
   User? user = FirebaseAuth.instance.currentUser;
